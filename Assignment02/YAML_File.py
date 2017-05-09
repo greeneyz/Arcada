122938aeb28b4acacbfe3e234b4cbec71f9f20bd
greeneyz <asim.ali.awan@gmail.com>
2017-05-09 16:04:55 +0300

    First Commit

---
+++ Assignment02/YAML_File.py
@@ -0,0 +1,44 @@
+
+# coding: utf-8
+
+# In[5]:
+
+import pandas as pd
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt2
+import matplotlib.pyplot as plt3
+import matplotlib.pyplot as plt4
+import datetime
+from pandas_datareader import data, wb # pip install pandas-datareader
+import yaml
+
+#path = "C:\Users\asim.ali.awan\OneDrive\Arcada\211028.yaml"
+with open('211028.yaml', 'r') as f:
+    doc = yaml.load(f)
+
+    strMatchInfo = doc['info']['city']
+    print (strMatchInfo)
+    print (doc)
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
